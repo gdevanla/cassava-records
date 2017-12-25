c57de7bf90ca9993a52d9cd34806c38aca988809
gdevanla <grdvnl@gmail.com>
2017-12-24 16:26:33 -0800

    add more type support

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,13 @@ module Main where
 
 import Data.Cassava.Records
 
-$(makeCsvRecord "NewR" "salaries.csv" "_csv")
+$(makeCsvRecord "NewR" "salaries.csv" "_csv" commaOptions)
+
+-- $(makeCsvRecord "NewR" "SEC_20170802.csv" "_csv" commaOptions)
+
+-- $(makeCsvRecord "NewR" "SEC_20170802_10.txt" "_tsv" tabOptions)
+
+-- $(makeCsvRecord "NewR" "SEC_20170802_101.csv" "_csv" commaOptions)
 
 main :: IO ()
 main = putStrLn ""
--- src/Data/Cassava/Records.hs
+++ src/Data/Cassava/Records.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Data.Cassava.Records where
 
@@ -13,7 +14,9 @@ import Data.List as L
 import Data.HashMap.Strict as H
 import Data.Csv hiding(Name)
 import Data.Attoparsec.ByteString as P
-
+import Data.String
+import Text.Read
+import Data.Char
 
 makeField:: String -> Name -> String -> (Name, Bang, Type)
 makeField fname ftype suffix = (
@@ -34,29 +37,83 @@ makeRecord record_name fields = do
       r = DataD [] (record_name') [] Nothing [recc] deriv
   return [r]
 
-createRecords csvData =
-  let p = CP.csvWithHeader CP.defaultDecodeOptions
+createRecords csvData options =
+  let p = CP.csvWithHeader options
       e = P.parseOnly p csvData in
     case e of
       Right f -> f
       Left f -> fail  $ "unable to parse" ++ f
 
 inferColumnType :: BL.ByteString -> V.Vector String -> (String, Name)
-inferColumnType header column = (BC.unpack header, ''Integer)
+inferColumnType header column = (BC.unpack header, inferMajorityType column)
 
-inferTypesForHeader :: BL.ByteString -> V.Vector NamedRecord -> V.Vector String
-inferTypesForHeader header named_records =
+isInteger s = case reads s :: [(Integer, String)] of
+    [(_, "")] -> True
+    _         -> False
+
+isDouble s = case reads s :: [(Double, String)] of
+    [(_, "")] -> True
+    _         -> False
+
+isNumeric :: String -> Bool
+isNumeric s = isInteger s || isDouble s
+
+-- isBool :: String -> Bool
+-- isBool c = let x = toLower c in
+--   x == "t" || x == "f" ||  x == "true" || x == "false"
+
+
+-- data Person = Person { name :: !Text, age :: !Int }
+
+-- instance FromNamedRecord Person where
+--   parseNamedRecord m = Person <$>
+--                        m .: "name" <*>
+--                        m .: "age"
+
+-- instance FromNamedRecord X where
+--   parseNamedRecord m = _
+
+-- class CRecords a where
+--   parseCField :: Field -> Parse a
+--   parseCField f = parseField f
+
+data Empty = Empty
+
+inferMajorityType column =
+  majority_types types'
+  where
+    types = V.map find_types column
+    types' = V.filter (\t -> t /= ''Empty) types
+    find_types c
+      | isNumeric c = ''Double
+--      | isBool c  = ''Bool
+      | c == "" = ''Empty
+      | otherwise = ''String
+    majority_types t1
+      | V.all (\t -> t == ''Double) t1 = ''Double
+      | V.all (\t -> t == ''Bool) t1 = ''Bool
+      | otherwise = ''String
+
+
+collectColumns :: BL.ByteString -> V.Vector NamedRecord -> V.Vector String
+collectColumns header named_records =
   V.map fn named_records where
   fn r = BC.unpack $ (r ! header)
 
 inferTypes :: Header -> V.Vector NamedRecord -> String -> V.Vector (Name, Bang, Type)
 inferTypes headers named_records suffix =
-  let columns = V.map (flip inferTypesForHeader named_records) headers
+  let columns = V.map (flip collectColumns named_records) headers
       fieldnames_types = makeFields (V.zipWith inferColumnType headers columns) suffix
   in
     fieldnames_types
 
-makeCsvRecord recordName fileName suffix = do
+tabOptions = defaultDecodeOptions {
+  decDelimiter = fromIntegral (ord '\t')
+  }
+
+commaOptions = defaultDecodeOptions
+
+makeCsvRecord recordName fileName suffix options = do
   csvData <- runIO $ BL.readFile fileName
-  let (headers, named_records) = createRecords csvData
+  let (headers, named_records) = createRecords csvData options
   makeRecord recordName (V.toList $ inferTypes headers named_records suffix)
