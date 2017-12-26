8bd9e65cc9db5777265f77ef4bcc36232cd5bcb1
gdevanla <grdvnl@gmail.com>
2017-12-26 06:03:17 -0800

    add support for Bool, clean up use of string

--- src/Data/Cassava/Records.hs
+++ src/Data/Cassava/Records.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 
+-- TODO
+-- support Bool, DateTime, Scientific
+-- Instances for ToNamedRecord, FromNamedRecord, DefaultOrdered
+-- accept options for instance creation
+
 module Data.Cassava.Records where
 
 import Control.Monad
@@ -9,24 +14,29 @@ import Language.Haskell.TH.Syntax
 import qualified Data.ByteString as BL
 import qualified Data.ByteString.Char8 as BC
 import Data.Csv.Parser as CP
+import qualified Data.Csv as Csv
 import qualified Data.Vector as V
 import Data.List as L
 import Data.HashMap.Strict as H
 import Data.Csv hiding(Name)
 import Data.Attoparsec.ByteString as P
+import Data.Attoparsec.Text as AT
 import Data.String
 import Text.Read
-import Data.Char
+import qualified Data.Char as DC
 import GHC.Generics (Generic)
 import Data.Text as DT
+import qualified Data.Text.Encoding as DTE
 
-makeField:: String -> Type -> String -> (Name, Bang, Type)
-makeField fname ftype suffix = (
-  mkName ("_" ++ fname), defaultBang , ftype)
+
+makeField:: BC.ByteString -> Type -> String -> (Name, Bang, Type)
+makeField fname ftype prefix = (
+  mkName (fname'), defaultBang , ftype)
   where
   defaultBang = Bang NoSourceUnpackedness NoSourceStrictness
+  fname' = prefix ++ (DT.unpack $ DT.toLower $ DTE.decodeUtf8 $ fname)
 
-makeFields:: V.Vector (String, Type) -> String -> V.Vector (Name, Bang, Type)
+makeFields:: V.Vector (BC.ByteString, Type) -> String -> V.Vector (Name, Bang, Type)
 makeFields fnames_types suffix = V.map makeField' fnames_types
   where
     makeField' = (\(f, t) -> makeField f t suffix)
@@ -39,6 +49,8 @@ makeRecord record_name fields = do
       r = DataD [] (record_name') [] Nothing [recc] deriv
   return [r]
 
+createRecords ::
+  BC.ByteString -> DecodeOptions -> (Header, V.Vector NamedRecord)
 createRecords csvData options =
   let p = CP.csvWithHeader options
       e = P.parseOnly p csvData in
@@ -46,8 +58,8 @@ createRecords csvData options =
       Right f -> f
       Left f -> fail  $ "unable to parse" ++ f
 
-inferColumnType :: BL.ByteString -> V.Vector String -> (String, Type)
-inferColumnType header column = (BC.unpack header, inferMajorityType column)
+inferColumnType :: BL.ByteString -> V.Vector BC.ByteString -> (BC.ByteString, Type)
+inferColumnType header column = (header, inferMajorityType column)
 
 isInteger s = case reads s :: [(Integer, String)] of
     [(_, "")] -> True
@@ -60,9 +72,20 @@ isDouble s = case reads s :: [(Double, String)] of
 isNumeric :: String -> Bool
 isNumeric s = isInteger s || isDouble s
 
--- isBool :: String -> Bool
--- isBool c = let x = toLower c in
---   x == "t" || x == "f" ||  x == "true" || x == "false"
+isBool :: String -> Bool
+isBool c = let x = fmap DC.toLower c
+  in
+  x == "t" || x == "f" ||  x == "true" || x == "false"
+
+instance ToField Bool where
+  toField True = "True"
+  toField False = "False"
+
+instance FromField Bool where
+  parseField field = do
+    let s' = DT.toLower . DTE.decodeUtf8 $ field
+    if s' == "t" || s' == "True" then return True
+      else return False
 
 
 -- data Person = Person { name :: !Text, age :: !Int }
@@ -85,6 +108,7 @@ data Empty = Empty
 
 maybeType ftype = AppT (ConT ''Maybe) (ConT ftype)
 
+inferMajorityType :: V.Vector BC.ByteString -> Type
 inferMajorityType column =
   majority_types types'
   where
@@ -92,23 +116,23 @@ inferMajorityType column =
     types' = V.filter (\t -> t /= ''Empty) types
     non_types' = V.filter (\t -> t == ''Empty) types
     find_types c
-      | isNumeric c = ''Double
---      | isBool c  = ''Bool
+      | isNumeric (DT.unpack . DTE.decodeUtf8 $ c) = ''Double
+      | isBool (DT.unpack . DTE.decodeUtf8 $ c)  = ''Bool
       | c == "" = ''Empty
-      | otherwise = ''String
+      | otherwise = ''Text
     majority_types t1
       | (V.all (\t -> t == ''Double) t1) && V.length non_types' > 0 = maybeType ''Double
       | V.all (\t -> t == ''Double) t1 = ConT ''Double
       | (V.all (\t -> t == ''Bool) t1) && V.length non_types' > 0 = maybeType ''Bool
       | V.all (\t -> t == ''Bool) t1 = ConT ''Bool
-      | V.length non_types' > 0 = maybeType ''String
-      | otherwise = ConT ''String
+      | V.length non_types' > 0 = maybeType ''Text
+      | otherwise = ConT ''Text
 
 
-collectColumns :: BL.ByteString -> V.Vector NamedRecord -> V.Vector String
+collectColumns :: BL.ByteString -> V.Vector NamedRecord -> V.Vector BC.ByteString
 collectColumns header named_records =
   V.map fn named_records where
-  fn r = BC.unpack $ (r ! header)
+  fn r = r ! header
 
 inferTypes :: Header -> V.Vector NamedRecord -> String -> V.Vector (Name, Bang, Type)
 inferTypes headers named_records suffix =
@@ -117,13 +141,18 @@ inferTypes headers named_records suffix =
   in
     fieldnames_types
 
+tabOptions :: DecodeOptions
 tabOptions = defaultDecodeOptions {
-  decDelimiter = fromIntegral (ord '\t')
+  decDelimiter = fromIntegral (DC.ord '\t')
   }
 
+
+commaOptions :: DecodeOptions
 commaOptions = defaultDecodeOptions
 
-makeCsvRecord recordName fileName suffix options = do
+makeCsvRecord ::
+  String -> FilePath -> String -> DecodeOptions -> Q [Dec]
+makeCsvRecord recordName fileName prefix decodeOptions = do
   csvData <- runIO $ BL.readFile fileName
-  let (headers, named_records) = createRecords csvData options
-  makeRecord recordName (V.toList $ inferTypes headers named_records suffix)
+  let (headers, named_records) = createRecords csvData decodeOptions
+  makeRecord recordName (V.toList $ inferTypes headers named_records prefix)
